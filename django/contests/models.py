f89ec281adb0e0a24d08af11601ebfe51986c50a
Emily <emily@nicehou.se>
2017-02-14 21:53:12 -0800

    more confusion

--- django/contests/models.py
+++ django/contests/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Bot(model.Model):
     title = models.CharField()
     status = models.CharField()
-    retweet_list =
-    retweeted_followed_list =
-    ignore_list =
+    retweet_list = models.ForeignKey()
+    retweete_follow_list = models.ForeignKey()
+    ignore_list = models.ForeignKey()
 
 
