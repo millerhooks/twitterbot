93e37a3fc220c917f68567dd371689aec6615002
Emily <emily@nicehou.se>
2017-02-14 21:49:45 -0800

    Confusion

---
+++ README.md
@@ -0,0 +1,5 @@
+365  git status
+  366  git add .
+  367  git commit -a -m "I spelled django wrong"
+  368  git push
+  369  ls
--- django/contests/models.py
+++ django/contests/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Bot(model.Model):
+    title = models.CharField()
+    status = models.CharField()
+    retweet_list =
+    retweeted_followed_list =
+    ignore_list =
+
+
